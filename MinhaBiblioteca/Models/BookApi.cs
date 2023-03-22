a1a062c209d2103297b1bfce57133af56ab11312
Antunigr <antuniribeiro@gmail.com>
2023-03-22 19:58:40 -0300

    apiService

---
+++ MinhaBiblioteca/Models/BookApi.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace MinhaBiblioteca.Models
+{
+    public class BookApi
+    {
+        [JsonProperty("title")]
+        public string titulo { get; set; }
+
+        [JsonProperty("authors")]
+        public string autor { get; set; }
+    }
+}
---
+++ MinhaBiblioteca/Services/BookApiService.cs
@@ -0,0 +1,20 @@
+﻿using MinhaBiblioteca.Models;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace MinhaBiblioteca.Services
+{
+    public class BookApiService
+    {
+        public async Task<BookApi>getNameBooks(string BookName)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync($"https://www.googleapis.com/books/v1/volumes?q={BookName}&key=AIzaSyCKyn2BShRjT4lzW7SxdXMcBhlnLdFb3G4");
+            var jsonString = await response.Content.ReadAsStringAsync();
+
+            BookApi jsonObject = JsonConvert.DeserializeObject<BookApi>(jsonString);
+
+            return jsonObject;
+        }
+    }
+}
