fc5cd41e0c5712bf639dd16f679ce5b5ec6d9340
Antunigr <antuniribeiro@gmail.com>
2023-03-22 18:28:44 -0300

    Adicionar arquivos de projeto.

---
+++ MinhaBiblioteca/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaBiblioteca.Models;
+using System.Diagnostics;
+
+namespace MinhaBiblioteca.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Biblioteca()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
