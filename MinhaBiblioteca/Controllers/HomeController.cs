bb97e92b8eefcd6f46b537026257335fd764c049
Antunigr <antuniribeiro@gmail.com>
2023-03-22 23:18:02 -0300

    ApiSucess

--- MinhaBiblioteca/Controllers/HomeController.cs
+++ MinhaBiblioteca/Controllers/HomeController.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MinhaBiblioteca.Models;
+using MinhaBiblioteca.Services;
 using System.Diagnostics;
 
 namespace MinhaBiblioteca.Controllers
 {
     public class HomeController : Controller
     {
+
+        public BookApiService bookservice = new BookApiService();
+
         //private readonly ILogger<HomeController> _logger;
 
         //public HomeController(ILogger<HomeController> logger)
@@ -18,9 +22,25 @@ namespace MinhaBiblioteca.Controllers
             return View();
         }
 
-        public IActionResult Biblioteca()
+        [HttpGet]
+        [Route("Home/Biblioteca/{nome?}")]
+        public async Task<IActionResult> Biblioteca(string? nome)
         {
+            if(nome == null)
+            {
+                List<BookApi> bookApis= new List<BookApi>();
+                ViewBag.book = bookApis;
+            }
+            else
+            {
+                List<BookApi> data = await bookservice.GetBookName(nome);
+              ViewBag.book = data;
+            }
+
+          
+
             return View();
+
         }
 
         public IActionResult Login()
@@ -30,3 +50,4 @@ namespace MinhaBiblioteca.Controllers
 
     }
 }
+
--- MinhaBiblioteca/Services/BookApiService.cs
+++ MinhaBiblioteca/Services/BookApiService.cs
@@ -6,15 +6,43 @@ namespace MinhaBiblioteca.Services
 {
     public class BookApiService
     {
-        public async Task<BookApi>GetBookName(string BookName)
+        public async Task<List<BookApi>>GetBookName(string? BookName)
         {
             HttpClient client = new HttpClient();
             var response = await client.GetAsync($"https://www.googleapis.com/books/v1/volumes?q={BookName}&key=AIzaSyCKyn2BShRjT4lzW7SxdXMcBhlnLdFb3G4");
             var jsonString = await response.Content.ReadAsStringAsync();
 
-            BookApi jsonObject = JsonConvert.DeserializeObject<BookApi>(jsonString);
+            var jsonObject = JsonConvert.DeserializeObject<RootObject>(jsonString);
+            var books = new List<BookApi>();
 
-            return jsonObject;
+            foreach (var item in jsonObject.items)
+            {
+                var book = new BookApi
+                {
+                    autor = item.volumeInfo.authors != null ? item.volumeInfo.authors[0] : null,
+                    titulo = item.volumeInfo.title
+                };
+                books.Add(book);
+            }
+
+            return books;
         }
     }
+
+    public class RootObject
+    {
+        public List<Item> items { get; set; }
+    }
+
+    public class Item
+    {
+        public VolumeInfo volumeInfo { get; set; }
+    }
+
+    public class VolumeInfo
+    {
+        public string title { get; set; }
+        public List<string> authors { get; set; }
+    }
+
 }
